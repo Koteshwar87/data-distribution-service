1effdf38bfafaa0f7496dd1358969fc718b1b1de
unknown <kotesh-full-access>
2026-01-04 18:36:16 +0530

    Main class in Api and Worker apps

---
+++ distribution-api/src/main/java/com/datadistribution/api/DataDistributionApiApplication.java
@@ -0,0 +1,11 @@
+package com.datadistribution.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataDistributionApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DataDistributionApiApplication.class, args);
+    }
+}
---
+++ distribution-worker/src/main/java/com/datadistribution/worker/DataDistributionWorkerApplication.java
@@ -0,0 +1,11 @@
+package com.datadistribution.worker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataDistributionWorkerApplication {
+    public static void main(String[] args){
+        SpringApplication.run(DataDistributionWorkerApplication.class, args);
+    }
+}
